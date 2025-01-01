496962282d6bdc4eeb20445f763f24bc062314df
loveCiForever <quanghuy71847@gmail.com>
2025-01-01 16:31:28 +0700

    add history price demo (use vnstock3) to visualize for client

--- client/src/datas/get_history_price_stock.py
+++ client/src/datas/get_history_price_stock.py
@@ -3,16 +3,17 @@ from vnstock3 import Vnstock
 
 tickers = ['ACB', 'FPT', 'VNM', 'VIC', 'VRE', 'ACV', 'AGR', 'BCG', 'BID']
 
-directory = "\\DemoStocks\\"
+directory = "DemoStocks\\"
 
 vnstock = Vnstock()
 
 for ticker in tickers:
     try:
-        data = vnstock.get_price(ticker)
-        if data is not None and not data.empty:
+        stock = Vnstock().stock(symbol=ticker, source='VCI')
+        df = stock.quote.history(start='2024-01-01', end='2024-12-31', interval='1D')
+        if df is not None and not df.empty:
             file_path = os.path.join(directory, ticker + ".csv")
-            data.to_csv(file_path)
+            df.to_csv(file_path, index=False)
             print(f"Saved {ticker} to {file_path}")
         else:
             print(f"Failed to get {ticker}")
