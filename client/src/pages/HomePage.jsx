0f2795f5324ae5f1cbeb487e3d385346aeae185a
loveCiForever <quanghuy71847@gmail.com>
2025-01-02 00:36:53 +0700

    add Footer and SearchBar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
-
 // Libraries and Hooks
 import { useEffect, useState } from "react";
 
 // Components
 import Header from "../components/header/Header";
 import SideBar from "../components/sideBar/SideBar";
+import Footer from "../components/footer/Footer";
+import SearchBar from "../components/searchBar/SearchBar";
 
 // Assets
-
-
-
 
 const HomePage = () => {
   useEffect(() => {
@@ -36,10 +34,21 @@ const HomePage = () => {
   }, [isSideBarOpen]);
 
   return (
-    <div>
-      <Header toggleSideBar={() => toggleSideBar(!isSideBarOpen)} />
-      {isSideBarOpen && <SideBar toggleSideBar={toggleSideBar} />}
+    <div className="flex flex-col items-center min-w-full min-h-screen bg-white">
+      <div className='fixed inset-x-0 min-w-[1000px] bg-red-00 w-full'>
+        <Header toggleSideBar={() => toggleSideBar(!isSideBarOpen)} />
+        {isSideBarOpen && <SideBar toggleSideBar={toggleSideBar} />}
+      </div>
       
+      <div className='flex items-center justify-center w-auto mt-20'>
+        
+      </div>
+
+      <SearchBar />
+
+      <div className=' w-full flex items-center justify-center'>
+        <Footer />
+      </div>
     </div>
   );
 };
