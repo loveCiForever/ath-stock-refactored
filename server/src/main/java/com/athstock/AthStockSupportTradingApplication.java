fe1ee6ac4e32bbb47868f8f767b725159d70d77a
loveCiForever <quanghuy71847@gmail.com>
2025-01-07 09:43:17 +0700

    switch from python flask to java spring boot

---
+++ server/src/main/java/com/athstock/AthStockSupportTradingApplication.java
@@ -0,0 +1,13 @@
+package com.athstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthStockSupportTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthStockSupportTradingApplication.class, args);
+	}
+
+}
--- server/ssi_api/dist/ssi_fc_data/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import api
-from . import model
-from . import constants
-from .access_token import AccessTokenModel
