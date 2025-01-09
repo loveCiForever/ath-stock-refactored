d7ab4244e3cd188e532588f85609a5b7c36212bd
loveCiForever <quanghuy71847@gmail.com>
2025-01-10 06:51:10 +0700

    temp commit

--- athstockserver/src/main/java/com/athstock/athstockserver/controller/UserController.java
+++ athstockserver/src/main/java/com/athstock/athstockserver/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.athstock.athstockserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.athstock.athstockserver.domain.User;
 import com.athstock.athstockserver.service.UserService;
+// import com.athstock.athstockserver.service.error.IdInvalidException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.lang.Exception;
+
+// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -15,15 +29,48 @@ public class UserController {
         this.userService = userService;
     }
     
-    @GetMapping("/user/create")
-    public String createNewUser() {
-        User user = new User();
-        user.setEmail("quanghuy71847@gmail.com");
-        user.setName("Nguyen Quang Huy");
-        user.setPassword("admin");
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(
+        @RequestBody User postmanUser
+    ) {
+        User newUser = this.userService.handleCreateUser(postmanUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
 
-        this.userService.handleCreateUser(user);
+    @ExceptionHandler(value = InInvalidException.class)
+    public ResponseEntity<String> handleException(InInvalidException idException) {
+        return ResponseEntity.badRequest().body("testing exception");
+    }
+    
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(
+        @PathVariable("id") Long id
+    ) {
+        this.userService.handleDeleteUser(id);
+        String msg = String.format("User with id %d has been deleted", id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(msg);
+        // return ResponseEntity.noContent();
+    }
 
-        return "User Createddddd";
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(
+        @PathVariable("id") Long id
+    ) {
+        User fetchedUser = this.userService.fetchUserById(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(fetchedUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> allUsers =  this.userService.fetchAllUser();
+        return ResponseEntity.status(HttpStatus.CREATED).body(allUsers);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser (
+        @RequestBody User reqUser
+    ) {
+        User updatedUser = this.userService.handleUpdateUser(reqUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedUser);
     }
 }
--- athstockserver/src/main/java/com/athstock/athstockserver/service/UserService.java
+++ athstockserver/src/main/java/com/athstock/athstockserver/service/UserService.java
@@ -1,9 +1,13 @@
 package com.athstock.athstockserver.service;
 
 
+import java.util.Optional;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
-import com.athstock.athstockserver.repository.UserRepository;
+
 import com.athstock.athstockserver.domain.User;
+import com.athstock.athstockserver.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -13,7 +17,35 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void handleCreateUser(User user) {
-        this.userRepository.save(user);
+    public User handleCreateUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(Long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public User fetchUserById(Long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        return userOptional.isPresent() ? userOptional.get() : null;
+    }
+
+    public List<User> fetchAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User reqUser) {
+        User currentUser = this.fetchUserById(reqUser.getId());
+
+        if(currentUser != null) {
+            currentUser.setEmail(reqUser.getEmail());
+            currentUser.setName(reqUser.getName());
+            currentUser.setPassword(reqUser.getPassword());
+
+            // update
+            currentUser = this.userRepository.save(currentUser);
+        }
+
+        return currentUser;
     }
 }
